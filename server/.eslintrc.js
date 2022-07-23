6825db976d464ffc63da55123c28e0d658fe1627
jcar23 <87565884+jcar23@users.noreply.github.com>
2022-07-23 13:14:41 +0100

    updates

---
+++ server/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+console.log('life is good')
+const express = require('express')// connection with the server http
+const bodyParser = require('body-parser')// to process json language easily 
+const cors = require('cors') // for a server to be host in a different domain name
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+app.listen(process.env.PORT || 8081)
+
+app.get("/status", (req,res)=>{
+    res.send({Message: 'hello worl'})
+})
